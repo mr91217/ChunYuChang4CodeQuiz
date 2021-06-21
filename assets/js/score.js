d56170dc727e5d4c12e4a512afb8977ac1e3b0b0
ChunYu Chang <lulutofulucas@ChunYus-MacBook-Air.local>
2021-06-21 09:50:20 -0400

    edit name

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,5 +1,5 @@
-var scoreshitname = localStorage.getItem("name")
-var scoreshit = localStorage.getItem("Highscore")
+var scoreusername = localStorage.getItem("name")
+var scorerecord = localStorage.getItem("Highscore")
 var h3 = document.createElement(h3);
-h3.textContent = "1." + scoreshitname + "---" + scoreshit;
+h3.textContent = "1." + scoreusername + "---" + scorerecord;
 scoreName.appendChild(h3);
